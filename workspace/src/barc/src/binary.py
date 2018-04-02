eab7d4bdf2f066ee575bb1c281d8de834449f0c6
Che Ho Chan <chehochan1986@gmail.com>
2018-04-01 21:02:44 -0500

    Grayscale and Contour
    The file turns the image to grayscale and find the contour.
    No line following implementation as of now.

---
+++ workspace/src/barc/src/binary.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+
+#read whatever image is received from webcam.
+image = cv2.imread('test.png', 0)
+
+ret, thresh = cv2.threshold(image, 127,255,0)
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+
+cnt = contours[0]
+M = cv2.moments(cnt)
+print (M)
