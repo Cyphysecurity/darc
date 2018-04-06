7c207a2c79b914ab6155c7b6661f1cf6cbc0dc64
Miguel Salinas <mikeys@me.com>
2018-04-06 14:23:22 -0500

    Indentation

--- workspace/src/barc/src/controller_camera.py
+++ workspace/src/barc/src/controller_camera.py
@@ -8,30 +8,30 @@ from barc.msg import LineData, ECU
 
 class LineFollowController:
 
-	def __init__(self):
+    def __init__(self):
 
-		self.subscriber = rospy.Subscriber("/line/ang_disp", LineData, self.callback_data)
+        self.subscriber = rospy.Subscriber("/line/ang_disp", LineData, self.callback_data)
 
-		self.publisher = rospy.Publisher("ecu", ECU, queue_size=10)
+        self.publisher = rospy.Publisher("ecu", ECU, queue_size=10)
 
 
-	def callback_data(self, data):
-		# Do all computation from the incoming message to the output message here
-		(angle, displacement) = data
+    def callback_data(self, data):
+        # Do all computation from the incoming message to the output message here
+        (angle, displacement) = data
 
 def main(args):
     global angle
     
     rospy.init_node("controller_camera") #initialize ros node
 
-	controller = LineFollowController()
-
-	while (1):
-		self.publisher.publish(ECU(20.0, angle + 90.0))
-		rospy.spin()
+    controller = LineFollowController()
+    
+    while (1):
+        self.publisher.publish(ECU(20.0, angle + 90.0))
+        rospy.spin()
 
 if __name__ == '__main__':
     try:
         main(sys.argv)
     except rospy.ROSInterruptException:
-	pass
+    pass
